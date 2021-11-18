ff8664f735041e2ebbcfc2d801663ae66405cc18
anakin1359 <anakin1359>
2021-11-18 23:07:03 +0900

    index.jsの初期設定

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>Hello Anakin.</h1>
+      <p>Hello Luke.</p>
+    </React.Fragment>
+  )
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
