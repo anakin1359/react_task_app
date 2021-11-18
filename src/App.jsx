8957c64534d4c0dcb26ec9f0c50b6201a62477d2
anakin1359 <anakin1359>
2021-11-18 23:58:49 +0900

    jsxにcssを適用

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const App = () => {
   const onClickButton = () => alert();
+  const contentStyle = {
+    color: 'blue',
+    fontSize: '20px'
+  }
   return (
     <>
-      <h1>Hello Anakin.</h1>
-      <p>Hello Luke.</p>
+      <h1 style={{ color: 'red' }}>Hello Anakin.</h1>
+      <p style={contentStyle}>Hello Luke.</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
