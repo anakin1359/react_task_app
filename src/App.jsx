87ff823aa8fc19a640305f1d0a94a3093e081319
anakin1359 <anakin1359>
2021-11-18 23:43:03 +0900

    jsxの戻り値(html構文)でjsを呼び出す処理を追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => alert();
   return (
     <>
       <h1>Hello Anakin.</h1>
       <p>Hello Luke.</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
-}
+};
 
 export default App;
