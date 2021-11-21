91e5aef01b7c57125b4f28e8f8a6d324bd7d5978
anakin1359 <anakin1359>
2021-11-22 01:12:46 +0900

    cssの設定

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,26 @@ import "./style.css";
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
-        <ul>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul className="list-row">
           <li>ゴリラ</li>
           <button>完了</button>
           <button>削除</button>
         </ul>
-        <ul>
+        <ul className="list-row">
           <li>カッパ</li>
           <button>完了</button>
           <button>削除</button>
         </ul>
       </div>
-      <div>
-        <p>完了のTODO</p>
-        <ul>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul className="list-row">
           <li>ウサギ</li>
           <button>戻す</button>
         </ul>
