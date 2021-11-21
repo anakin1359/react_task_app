3123738fb90ad2fe9f1fa2bc26268190d8ad473a
anakin1359 <anakin1359>
2021-11-22 00:18:11 +0900

    useEffect機能を実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("ゴリラ");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
 
-  const [num, setNum] = useState(0);
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  /* 3の倍数の時だけ顔文字を表示（0の時は非表示） */
+  useEffect(() => {
+    /* console.log("useEffect!!"); */
+    if (num > 0 && num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    } else {
+      faceShowFlag && setFaceShowFlag(false);
+    };
+  /* eslint react-hooks/exhaustive-deps: off */
+  }, [num]);
 
   return (
     <>
-      <h1 style={{ color: 'red' }}>Hello Anakin.</h1>
-      <ColorfulMessage color="blue">May the force be with you.</ColorfulMessage>
-      <ColorfulMessage color="pink">No! Try not. Do. Or do not. There is no try.</ColorfulMessage>
+      <h1 style={{ color: 'red' }}>おはようございます</h1>
+      <ColorfulMessage color="blue">こんにちは</ColorfulMessage>
+      <ColorfulMessage color="pink">いただきます</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(｀・∀・´)</p>}
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
-  console.log(props);
-
   const { color, children } = props;
   const contentStyle = {
     color: color,
