f03aebf6bd56daeb1e12bafd0dd257ffa89007ea
anakin1359 <anakin1359>
2021-11-22 01:34:14 +0900

    useStateの設定を追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 
 export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "ゴリラ",
+    "カッパ"
+  ]);
+
+  const [completeTodos, setInCompleteTodos] = useState(["ウサギ"]);
+
   return (
     <>
       <div className="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
+
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
-        <ul className="list-row">
-          <li>ゴリラ</li>
-          <button>完了</button>
-          <button>削除</button>
-        </ul>
-        <ul className="list-row">
-          <li>カッパ</li>
-          <button>完了</button>
-          <button>削除</button>
-        </ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              <ul key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </ul>
+            );
+          })}
       </div>
+
       <div className="complete-area">
         <p className="title">完了のTODO</p>
-        <ul className="list-row">
-          <li>ウサギ</li>
-          <button>戻す</button>
-        </ul>
+          {completeTodos.map((todo) => {
+            return (
+              <ul key={todo} className="list-row">
+                <li>ウサギ</li>
+                <button>戻す</button>
+              </ul>
+            );
+          })}
       </div>
     </>
   );
