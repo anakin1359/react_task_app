d860fc3d3cee3e2eb47ef2a6590bfc470214fad1
anakin1359 <anakin1359>
2021-11-22 01:07:23 +0900

    App.jsxの初期設定

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,32 @@ import React from "react";
 import "./style.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>ゴリラ</li>
+          <button>完了</button>
+          <button>削除</button>
+        </ul>
+        <ul>
+          <li>カッパ</li>
+          <button>完了</button>
+          <button>削除</button>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <li>ウサギ</li>
+          <button>戻す</button>
+        </ul>
+      </div>
+    </>
+  );
 };
