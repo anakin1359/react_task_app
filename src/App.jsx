46fae407a7ac33eee9350920f1aab7e0b5fe8b10
anakin1359 <anakin1359>
2021-11-21 22:49:10 +0900

    state機能(onClickCountUp)を実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+
+  const [num, setNum] = useState(0);
 
   return (
     <>
       <h1 style={{ color: 'red' }}>Hello Anakin.</h1>
       <ColorfulMessage color="blue">May the force be with you.</ColorfulMessage>
       <ColorfulMessage color="pink">No! Try not. Do. Or do not. There is no try.</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
