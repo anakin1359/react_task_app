db5bf695352ef2c18bff3b5569c893faa31ba0ba
anakin1359 <anakin1359>
2021-11-25 05:15:07 +0900

    完了ボタン押下時のアクションを実装(未完了のTODO →  完了のTODO)

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export const App = () => {
     "カッパ"
   ]);
 
-  const [completeTodos, setInCompleteTodos] = useState([
+  const [completeTodos, setCompleteTodos] = useState([
     "ウサギ"
   ]);
 
@@ -27,6 +27,14 @@ export const App = () => {
     setIncompleteTodos(newTodos);
   };
 
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -40,7 +48,7 @@ export const App = () => {
             return (
               <ul key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
                 <button onClick={() => onClickDelete(index)}>削除</button>
               </ul>
             );
