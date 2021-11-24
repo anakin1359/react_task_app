5c9726090619c8b2135c100e39ed60cb0457cc11
anakin1359 <anakin1359>
2021-11-25 04:41:31 +0900

    削除ボタン押下時のアクションを実装(未完了リストから指定した項目を削除)

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,12 @@ export const App = () => {
     setTodoText("");
   };
 
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -30,12 +36,12 @@ export const App = () => {
 
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <ul key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </ul>
             );
           })}
